5c6122d21969f8b448678918e95b8fcfceee6337
raphdom <raphael.rodrigues-domingues@ext.ec.europa.eu>
2020-02-17 10:55:57 +0100

    add javascript to generate the points file

---
+++ src/generateMapPoints.js
@@ -0,0 +1,57 @@
+const axios = require('axios');
+const fs = require('fs');
+
+const server = 'http://query.linkedopendata.eu/bigdata/namespace/wdq/sparql';
+const query = 'select (REPLACE(STR(?project),".*Q","") AS ?projectid) ?countrycode ?coordinates where {' +
+    '?project <https://linkedopendata.eu/prop/direct/P35> <https://linkedopendata.eu/entity/Q9934> .' +
+    '?project <https://linkedopendata.eu/prop/direct/P32> ?country .' +
+    '?country <https://linkedopendata.eu/prop/direct/P173> ?countrycode .' +
+    '?project <https://linkedopendata.eu/prop/direct/P127> ?coordinates .' +
+    '}';
+
+class GenerateMapPoints{
+
+    constructor() {
+
+    }
+
+    run() {
+        console.info("INFO: Getting the points in API and placed into a static file");
+        const url = server + '?query=' + encodeURI(query);
+        axios({
+            method: 'get',
+            url: url
+        })
+            .then(function (response) {
+                const points = response.data.results.bindings;
+                let resultJSON = {};
+                console.log("INFO: Generate " + points.length + " points");
+                for(let point of points){
+                    if (!resultJSON[point.countrycode.value]){
+                        resultJSON[point.countrycode.value] = [];
+                    }
+                    const coordinates = point.coordinates.value.replace("Point(", "")
+                        .replace(")","")
+                        .split(" ");
+                    resultJSON[point.countrycode.value].push(
+                        [
+                            point.projectid.value,
+                            coordinates[0],
+                            coordinates[1]
+                        ]
+                    )
+                }
+                fs.writeFile('public/points.json', JSON.stringify(resultJSON), (err) => {
+                    if (err) throw err;
+                    console.log('INFO: File points.json is created successfully.');
+                });
+            })
+            .catch(function (error) {
+                console.log(error);
+            });
+    }
+
+}
+
+new GenerateMapPoints().run();
+
