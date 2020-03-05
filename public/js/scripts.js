147863ec222aa553ea634cacf7bd3745c9f7ecd9
raphdom <raphael.rodrigues-domingues@ext.ec.europa.eu>
2020-03-05 16:03:19 +0100

    add the category icons

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -1,5 +1,31 @@
 let countrySelect;
 let topicSelect;
+const months = [
+    'January',
+    'February',
+    'March',
+    'April',
+    'May',
+    'June',
+    'July',
+    'August',
+    'September',
+    'October',
+    'November',
+    'December'
+];
+function formatBudget (value) {
+    // Nine Zeroes for Billions
+    return Math.abs(Number(value)) >= 1.0e+9
+        ? Math.abs(Number(value)) / 1.0e+9 + "B"
+        // Six Zeroes for Millions
+        : Math.abs(Number(value)) >= 1.0e+6
+            ? Math.abs(Number(value)) / 1.0e+6 + "M"
+            // Three Zeroes for Thousands
+            : Math.abs(Number(value)) >= 1.0e+3
+                ? Math.abs(Number(value)) / 1.0e+3 + "K"
+                : Math.abs(Number(value));
+}
 
 fetch('filters.json')
     .then(response => {
@@ -31,7 +57,9 @@ fetch('filters.json')
         const topicsData = [];
         for(let topic of json.topics){
             topicsData.push({
-                text: topic[1]
+                value: topic[0],
+                text: topic[1],
+                innerHTML: '<img src="images/'+topic[0]+'_80.png">'+topic[1]
             })
         }
         topicSelect.setData(topicsData);
@@ -39,6 +67,73 @@ fetch('filters.json')
     .catch(function () {
         this.dataError = true;
     });
+
+//Getting the project details
+const server = 'https://query.linkedopendata.eu/bigdata/namespace/wdq/sparql';
+const queryProjects = 'SELECT DISTINCT ?s0 ?label ?description ?startTime ?euBudget ?image ?coordinates ?objectiveId WHERE { ' +
+    '  ?s0 <https://linkedopendata.eu/prop/direct/P35> <https://linkedopendata.eu/entity/Q9934>. ' +
+    '  { ' +
+    '    ?s0 rdfs:label ?label. ' +
+    '    FILTER((LANG(?label)) = "en") ' +
+    '  }' +
+    '  {' +
+    '    ?s0 <https://linkedopendata.eu/prop/direct/P836> ?description. ' +
+    '    FILTER((LANG(?description)) = "en")' +
+    '  }' +
+    '  { ?s0 <https://linkedopendata.eu/prop/direct/P20> ?startTime. } ' +
+    '  { ?s0 <https://linkedopendata.eu/prop/direct/P835> ?euBudget. }' +
+    '  OPTIONAL { ?s0 <https://linkedopendata.eu/prop/direct/P147> ?image. }' +
+    '  OPTIONAL { ?s0 <https://linkedopendata.eu/prop/direct/P851> ?image. }' +
+    '  { ?s0 <https://linkedopendata.eu/prop/direct/P127> ?coordinates. }' +
+    '  { ?s0 <https://linkedopendata.eu/prop/direct/P888> ?category. }' +
+    '  { ?category <https://linkedopendata.eu/prop/direct/P302> ?objective. }' +
+    '  { ?objective <https://linkedopendata.eu/prop/direct/P1105> ?objectiveId. }' +
+    '}' +
+    'LIMIT 11';
+const urlProjects = encodeURI(server + '?query=' + queryProjects);
+fetch(urlProjects,{
+    headers: {
+        'Accept': 'application/sparql-results+json'
+    }
+}).then(response => {
+    if (!response.ok) {
+        throw new Error("HTTP error " + response.status);
+    }
+    return response.json();
+}).then(json => {
+    let html = '';
+    let i = 1;
+    for (let project of json.results.bindings){
+        //let objectiveId = (i<10 ? "TO0" : "TO") + i;
+        let objectiveId = project.objectiveId.value;
+        let title = project.label.value.length > 60 ?
+            project.label.value.substring(0,60) + '...' :
+            project.label.value;
+        let description = project.description.value.length > 500 ?
+            project.description.value.substring(0,500) + '...' :
+            project.description.value;
+        const startTime = new Date(project.startTime.value);
+        const startTimeFormatted = months[startTime.getMonth()] + ' ' + startTime.getFullYear();
+        let budget = formatBudget(parseInt(project.euBudget.value)) + '€';
+        html +=
+            '<div class="card '+objectiveId + '">' +
+                '<div class="header">' +
+                    /*'<img src="images/TO'+project.objectiveId.value+'_80.png">' +*/
+                    '<img src="images/'+objectiveId+'_80.png">' +
+                    '<h1>' + title +'</h1>' +
+                '</div>' +
+                '<div class="info-bar">' +
+                    '<div class="startDate">'+ startTimeFormatted + '</div>' +
+                    '<div class="budget">'+ budget +'</div>' +
+                '</div>' +
+                '<p>' + description + '</p>' +
+            '</div>';
+        i++;
+    }
+    document.getElementById('projects-grid').innerHTML = html;
+}).catch(function () {
+
+});
 
 function onSearchClick(){
     const countriesSelected = countrySelect.selected();
