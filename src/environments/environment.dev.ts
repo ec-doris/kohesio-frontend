d6c27622964f714d3272d9d9a16a6ebe07afdb43
Raphael Domingues <raphdom@gmail.com>
2020-03-23 16:38:18 +0100

    new version with the full search text implemented

--- src/environments/environment.dev.ts
+++ src/environments/environment.dev.ts
@@ -7,5 +7,5 @@ export const environment = {
     production: false,
     enableDevToolRedux: true,
     sparqlBaseUrl: 'https://query.linkedopendata.eu/bigdata/namespace/wdq/sparql',
-    fullTextSearchUrl: 'http://qanswer-core1.univ-st-etienne.fr/api/endpoint/eu/sparql?user=Max'
+    fullTextSearchUrl: 'https://qanswer-core1.univ-st-etienne.fr/api/endpoint/eu/sparql?user=Max'
 };
