4a5df0cc510bfdf49c212c04c2da537fac1ad753
Raphael Domingues <raphdom@gmail.com>
2020-03-24 11:37:08 +0100

    fixing some bugs

--- src/app/features/home/home.component.ts
+++ src/app/features/home/home.component.ts
@@ -59,12 +59,6 @@ export class HomeComponent implements AfterViewInit {
     }
 
     ngAfterViewInit(): void {
-        this.map = L.map('map-inside').setView([48, 4], 5);
-        const tiles = L.tileLayer('https://europa.eu/webtools/maps/tiles/osmec2/{z}/{x}/{y}', {
-            attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors ' +
-                '| &copy; <a href="https://ec.europa.eu/eurostat/web/gisco">GISCO</a>'
-        });
-        tiles.addTo(this.map);
     }
 
     private getProjectList(filters:Filters){
@@ -79,6 +73,15 @@ export class HomeComponent implements AfterViewInit {
         this.projects = [];
         const filters = new Filters().deserialize(form.value);
         this.getProjectList(filters);
+    }
+
+    onMapModalAnimationEnd(event){
+        this.map = L.map('map-inside').setView([48, 4], 5);
+        const tiles = L.tileLayer('https://europa.eu/webtools/maps/tiles/osmec2/{z}/{x}/{y}', {
+            attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors ' +
+                '| &copy; <a href="https://ec.europa.eu/eurostat/web/gisco">GISCO</a>'
+        });
+        tiles.addTo(this.map);
     }
 
 }
