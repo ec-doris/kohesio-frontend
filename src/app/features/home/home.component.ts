ad669f14b1303e4f6514df494925281c4c642ed7
raphdom@gmail.com <kms387r12>
2020-06-02 14:52:29 +0200

    homepage layout

--- src/app/features/home/home.component.ts
+++ src/app/features/home/home.component.ts
@@ -1,10 +1,13 @@
 import {AfterViewInit, Component} from '@angular/core';
+declare let L;
 
 @Component({
-    templateUrl: './home.component.html'
+    templateUrl: './home.component.html',
+    styleUrls: ['./home.component.scss']
 })
 export class HomeComponent implements AfterViewInit {
 
+    public map;
 
     constructor(){}
 
@@ -12,6 +15,16 @@ export class HomeComponent implements AfterViewInit {
     }
 
     ngAfterViewInit(): void {
+        if (!this.map) {
+            this.map = L.map('map-inside').setView([46, 4], 4);
+            const tiles = L.tileLayer('https://europa.eu/webtools/maps/tiles/osmec2/{z}/{x}/{y}', {
+                attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors ' +
+                    '| &copy; <a href="https://ec.europa.eu/eurostat/web/gisco">GISCO</a>'
+            });
+            tiles.addTo(this.map);
+
+            //this.markerService.makeMarkers(this.map);
+        }
     }
 
 }
