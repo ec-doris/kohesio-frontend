ce6042058a7c93b7d1c5243875a048b29b7401ac
raphdom@gmail.com <kms387r12>
2020-06-03 16:22:45 +0200

    map plugin from ec

--- src/app/features/home/home.component.ts
+++ src/app/features/home/home.component.ts
@@ -1,5 +1,7 @@
 import {AfterViewInit, Component} from '@angular/core';
 declare let L;
+import { Renderer2, Inject } from '@angular/core';
+import { DOCUMENT } from '@angular/common';
 
 @Component({
     templateUrl: './home.component.html',
@@ -9,14 +11,12 @@ export class HomeComponent implements AfterViewInit {
 
     public map;
 
-    constructor(){}
-
-    ngOnInit(){
-    }
+    constructor(private _renderer2: Renderer2,
+                @Inject(DOCUMENT) private _document: Document){}
 
     ngAfterViewInit(): void {
-        if (!this.map) {
-            this.map = L.map('map-inside').setView([46, 4], 4);
+        /*if (!this.map) {
+            this.map = L.map('map').setView([46, 4], 4);
             const tiles = L.tileLayer('https://europa.eu/webtools/maps/tiles/osmec2/{z}/{x}/{y}', {
                 attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors ' +
                     '| &copy; <a href="https://ec.europa.eu/eurostat/web/gisco">GISCO</a>'
@@ -24,7 +24,65 @@ export class HomeComponent implements AfterViewInit {
             tiles.addTo(this.map);
 
             //this.markerService.makeMarkers(this.map);
-        }
+        }*/
+    }
+
+    public ngOnInit() {
+
+        let script = this._renderer2.createElement('script');
+        script.type = `application/json`;
+        script.text = `
+            {
+                "service": "map",
+                "renderTo" : "map",
+                "map": {
+                    "center": [
+                        46,
+                        4
+                    ],
+                    "zoom": 4,
+                    "background": [
+                        "osmec"
+                    ],
+                    "maxZoom": 18
+                },
+                "version": "2.0",
+                "layers": [
+                {
+                "countries": [
+                "PT",
+                "FR",
+                "ES"
+                ],
+                "options": {
+                "insets": {
+                "collapse": true
+                },
+                "label": {
+                "mode": "hover"
+                },
+                "style": {
+                "color": "#ff5454"
+                }
+                }
+                },{
+                "countries": [
+                "TR"
+                ],
+                "options": {
+                "label": {
+                "mode": "hover"
+                },
+                "style": {
+                "color": "#4bff5d"
+                }
+                }
+                }
+                ]
+            }
+        `;
+
+        this._renderer2.appendChild(this._document.body, script);
     }
 
 }
