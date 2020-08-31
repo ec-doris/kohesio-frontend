6ecbd79ffdce8a0f95db421aa99a1dba34d365b3
raphdom@gmail.com <kms387r12>
2020-08-31 08:58:09 +0200

    new environment and fix small bug

---
+++ src/environments/environment.acc.ts
@@ -0,0 +1,11 @@
+// activated by running :
+// npm run build -- --configuration=acc
+// OR
+// npm run build-acc
+
+export const environment = {
+    production: false,
+    enableDevToolRedux: false,
+    api: 'https://kohesio.linkedopendata.eu/api/dev/facet/eu',
+    entityURL: 'https://linkedopendata.eu/entity/'
+};
