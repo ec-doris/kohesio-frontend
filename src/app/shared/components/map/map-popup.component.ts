da2607c7f2b1afb6d9c4768a23507b63d74ad5d8
raphdom@gmail.com <kms387r12>
2020-11-11 13:37:20 +0100

    responsive design

--- src/app/shared/components/map/map-popup.component.ts
+++ src/app/shared/components/map/map-popup.component.ts
@@ -1,12 +1,12 @@
 import {AfterViewInit, Component, ViewChild, Input} from '@angular/core';
 
 @Component({
-    templateUrl: './map-popup.component.html'
+    templateUrl: './map-popup.component.html',
+    styleUrls: ['./map-popup.component.scss']
 })
 export class MapPopupComponent {
 
     @Input() projects: any[];
-
 
     constructor(){}
 
