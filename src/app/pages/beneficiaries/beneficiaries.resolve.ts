35e339579991af79c0e358ade48905f9e6c31e15
Omar Perez <ometh2@gmail.com>
2022-02-15 14:09:34 +0000

    Modified return value on catches to re-enable redirection to 404 page.

--- src/app/pages/beneficiaries/beneficiaries.resolve.ts
+++ src/app/pages/beneficiaries/beneficiaries.resolve.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { ActivatedRouteSnapshot, RouterStateSnapshot, Resolve, Router } from '@angular/router';
-import {Observable} from "rxjs";
+import {EMPTY, Observable} from "rxjs";
 import {catchError} from 'rxjs/operators';
 import {FilterService} from "../../services/filter.service";
 import {FiltersApi} from "../../models/filters-api.model";
@@ -12,12 +12,11 @@ export class BeneficiariesListResolver implements Resolve<FiltersApi> {
                 private router: Router) {}
 
     resolve(route: ActivatedRouteSnapshot,state: RouterStateSnapshot): Observable<FiltersApi> {
-        return this.service.getBeneficiariesFilters().pipe();
-        //TODO ECL side effect
-            // catchError(err => {
-            //     console.error(err);
-            //     this.router.navigate(["/404"]);
-            //     return Observable.of(null);
-            // }));
+        return this.service.getBeneficiariesFilters().pipe(
+            catchError(err => {
+                console.error(err);
+                this.router.navigate(["/404"]);
+                return EMPTY;
+            }));
     }
 }
