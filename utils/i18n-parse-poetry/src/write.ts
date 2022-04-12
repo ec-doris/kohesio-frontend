c19afe59f0f4442a6929dd0dbc78c577b057a3b1
Raphael Domingues <raphdom@gmail.com>
2022-04-12 14:46:06 +0000

    parse message file to poetry

--- utils/i18n-parse-poetry/src/write.ts
+++ utils/i18n-parse-poetry/src/write.ts
@@ -1,28 +1,41 @@
 const writeXlsxFile = require('write-excel-file/node');
+import messages from '../../../src/locale/messages.json'; 
 
 class Write {
 
     public async run(){
-        const data = [
-            // Row #1
-            [
-              // Column #1
-              {
-                value: 'Name',
-              }
-            ],
-            // Row #2
-            [
-              // Column #1
-              {
-                value: 'John Smith'
-              }
-            ]
-        ];
-
+        /*DATA*/
+        let data:any = [];
+        let index = 1;
+        for (const [key, value] of Object.entries(messages.translations)) {
+            data.push({
+                index: index,
+                code: key,
+                label: value
+            });
+            index++;
+        }
+        /*SCHEMA*/
+        const schema = [{
+            column: 'Index',
+            type: Number,
+            align: 'center',
+            value: (obj:any) => obj.index
+        },{
+            column: 'Code',
+            type: String,
+            color: '#FF0000',
+            value: (obj:any) => obj.code
+        },{
+            column: 'EN',
+            type: String,
+            value: (obj:any) => obj.label
+        }];
+        /*WRITING THE FILE*/
         await writeXlsxFile(data, {
+            schema,
             filePath: 'output/file.xlsx'
-          })
+        })
     }
 
 }
