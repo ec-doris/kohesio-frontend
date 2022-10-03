fab2cf6f63597433a082e2268fa5bd85ba60a528
Raphael Domingues <raphdom@gmail.com>
2022-10-03 13:41:53 +0200

    Starting i18n implementation

--- src/app/components/ecl/language-selector/language-selector.ecl.component.ts
+++ src/app/components/ecl/language-selector/language-selector.ecl.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import {Component, Inject, LOCALE_ID} from '@angular/core';
 
 
 @Component({
@@ -6,6 +6,8 @@ import { Component } from '@angular/core';
     templateUrl: './language-selector.ecl.component.html',
 })
 export class KohesioEclLanguageSelectorComponent {
+
+  constructor(@Inject(LOCALE_ID) public locale: string){}
 
   changeLanguage(selectedLangCode:string){
     window.location.href = `/${selectedLangCode}`
