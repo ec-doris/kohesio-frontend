561fab03d546ea6fee51d09a9fc36f13559ae38a
Raphael Domingues <raphdom@gmail.com>
2022-10-05 15:22:05 +0200

    change theme for homepage, by language

--- src/app/services/theme.service.ts
+++ src/app/services/theme.service.ts
@@ -11,7 +11,9 @@ import { environment } from 'src/environments/environment';
 })
 export class ThemeService {
 
-    constructor(private http: HttpClient, @Inject(LOCALE_ID) public locale: string) {}
+    constructor(private http: HttpClient, @Inject(LOCALE_ID) public locale: string) {
+
+    }
 
     getThemes(): Observable<Theme[]> {
         const url = environment.apiBaseUrl + '/thematic_objectives';
