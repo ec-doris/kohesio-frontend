c3dba66d63a69b03e8582c2ff4ef27ab27ca27d6
Raphael Domingues <raphdom@gmail.com>
2022-10-10 15:34:58 +0200

    more translations

--- src/app/services/translate.service.ts
+++ src/app/services/translate.service.ts
@@ -18,6 +18,36 @@ export class TranslateService {
     map: $localize`:@@translate.routes.map:map`
   }
 
+  public queryParams = {
+    "keywords": $localize`:@@translate.queryParams.keywords:keywords`,
+    "country": $localize`:@@translate.queryParams.country:country`,
+    "region": $localize`:@@translate.queryParams.region:region`,
+    "policyObjective": $localize`:@@translate.queryParams.policyObjective:policyObjective`,
+    "theme": $localize`:@@translate.queryParams.theme:theme`,
+    "fund": $localize`:@@translate.queryParams.fund:fund`,
+    "interreg": $localize`:@@translate.queryParams.interreg:interreg`,
+    "programme": $localize`:@@translate.queryParams.programme:programme`,
+    "totalProjectBudget": $localize`:@@translate.queryParams.totalProjectBudget:totalProjectBudget`,
+    "amountEUSupport": $localize`:@@translate.queryParams.amountEUSupport:amountEUSupport`,
+    "interventionField": $localize`:@@translate.queryParams.interventionField:interventionField`,
+    "nuts3": $localize`:@@translate.queryParams.nuts3:nuts3`,
+    "projectStart": $localize`:@@translate.queryParams.projectStart:projectStart`,
+    "projectEnd": $localize`:@@translate.queryParams.projectEnd:projectEnd`,
+    "sort": $localize`:@@translate.queryParams.sort:sort`,
+    "name": $localize`:@@translate.queryParams.name:name`,
+    "beneficiaryType": $localize`:@@translate.queryParams.beneficiaryType:beneficiaryType`,
+    "page": $localize`:@@translate.queryParams.page:page`,
+    "tab": $localize`:@@translate.queryParams.tab:tab`
+  }
+
+  public projectPage = {
+    tabs: {
+      results: $localize`:@@translate.projectPage.tabs.results:results`,
+      audiovisual: $localize`:@@translate.projectPage.tabs.audiovisual:audiovisual`,
+      map: $localize`:@@translate.projectPage.tabs.map:map`,
+    }
+  }
+
   constructor(private http: HttpClient, @Inject(LOCALE_ID) public locale: string) {}
 
 }
