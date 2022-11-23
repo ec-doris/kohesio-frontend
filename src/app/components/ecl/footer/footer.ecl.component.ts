f381f551e52e544521e70cfb2ed64681209268b3
Raphael Domingues <raphdom@gmail.com>
2022-11-23 10:19:13 +0100

    change regional policy url

--- src/app/components/ecl/footer/footer.ecl.component.ts
+++ src/app/components/ecl/footer/footer.ecl.component.ts
@@ -1,4 +1,4 @@
-import {AfterViewInit, Component} from '@angular/core';
+import {AfterViewInit, Component, Inject, LOCALE_ID} from '@angular/core';
 import {TranslateService} from "../../../services/translate.service";
 
 @Component({
@@ -10,7 +10,8 @@ import {TranslateService} from "../../../services/translate.service";
 export class FooterEclComponent implements AfterViewInit {
 
 
-    constructor(public translateService: TranslateService){}
+    constructor(public translateService: TranslateService,
+                @Inject(LOCALE_ID) public locale: string){}
 
     ngOnInit(){
     }
