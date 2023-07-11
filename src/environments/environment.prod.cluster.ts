86c729e4a11439c11064392a42d7002d684346ce
Raphael Domingues <raphdom@gmail.com>
2023-07-11 10:50:27 +0200

    env for PROD cluster

--- src/environments/environment.prod.cluster.ts
+++ src/environments/environment.prod.cluster.ts
@@ -4,7 +4,8 @@
 
 export const environment = {
   production: true,
-  apiBaseUrl: "http://kohesio-backend-service/api/facet/eu",
+  apiBaseUrl: "http://kohesio-backend-service/wikibase/facet/eu",
+  api:'http://kohesio-frontend-service/api',
   entityURL: 'https://linkedopendata.eu/entity/'
 };
 
