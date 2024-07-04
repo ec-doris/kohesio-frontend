a55289635676923d773655f52098ee74b6e6c8e4
nikolay torbev <ntorbev@gmail.com>
2024-07-04 15:33:53 +0300

    ang update

--- server.ts
+++ server.ts
@@ -1,32 +1,24 @@
+
 import 'zone.js/node';
 
 import { APP_BASE_HREF } from '@angular/common';
-import { ngExpressEngine } from '@nguniversal/express-engine';
+import { CommonEngine } from '@angular/ssr';
 import * as express from 'express';
-import { existsSync } from 'fs';
-import { join } from 'path';
-import { REQUEST, RESPONSE } from '@nguniversal/express-engine/tokens';
-
-import { AppServerModule } from './src/main.server';
-import {LOCALE_ID} from "@angular/core";
-import * as cookieParser from 'cookie-parser';
+import { existsSync } from 'node:fs';
+import { join } from 'node:path';
+import {  AppServerModule as bootstrap } from './src/main.server';
+import { REQUEST, RESPONSE } from './src/express.tokens';
 
 // The Express app is exported so that it can be used by serverless Functions.
-export function app(lang:string, dir:string): express.Express {
-
+export function app(): express.Express {
   const server = express();
+  const distFolder = join(process.cwd(), 'dist/kohesio-frontend/browser');
+  const indexHtml = existsSync(join(distFolder, 'index.original.html'))
+    ? join(distFolder, 'index.original.html')
+    : join(distFolder, 'index.html');
 
-  server.use(cookieParser());
+  const commonEngine = new CommonEngine();
 
-  const distFolder = join(dir ? dir : process.cwd(), `dist/kohesio-frontend/browser/${lang}`);
-  const indexHtml = existsSync(join(distFolder, 'index.original.html')) ? 'index.original.html' : 'index';
-
-  // Our Universal express-engine (found @ https://github.com/angular/universal/tree/main/modules/express-engine)
-  server.engine('html', ngExpressEngine({
-    bootstrap: AppServerModule,
-    providers: [{ provide: LOCALE_ID, useValue: lang}]
-  } as any));
-  //console.log("SERVER.ts-LANG="+lang);
   server.set('view engine', 'html');
   server.set('views', distFolder);
 
@@ -37,44 +29,47 @@ export function app(lang:string, dir:string): express.Express {
     maxAge: '1y'
   }));
 
-  // All regular routes use the Universal engine
-  server.get('*', (req, res) => {
-    res.render(indexHtml, { req, providers: [
-      { provide: APP_BASE_HREF, useValue: req.baseUrl },
-      { provide: REQUEST, useValue: req },
-      { provide: RESPONSE, useValue: res }
-      ] });
+  // All regular routes use the Angular engine
+  server.get('*', (req, res, next) => {
+    const { protocol, originalUrl, baseUrl, headers } = req;
+
+    commonEngine
+      .render({
+        bootstrap,
+        documentFilePath: indexHtml,
+        url: `${protocol}://${headers.host}${originalUrl}`,
+        publicPath: distFolder,
+        providers: [
+          { provide: APP_BASE_HREF, useValue: baseUrl },
+          { provide: RESPONSE, useValue: res },
+          { provide: REQUEST, useValue: req }
+],
+      })
+      .then((html) => res.send(html))
+      .catch((err) => next(err));
   });
 
   return server;
 }
 
-/*function run(): void {
-  console.log("RUN SERVER.ts");
-  const port = process.env['PORT'] || 4200;
-  const appFr = app('fr');
-  const appPt = app('pt');
-  const appEn = app('en');
+function run(): void {
+  const port = process.env['PORT'] || 4000;
 
   // Start up the Node server
-  const server = express();
-  server.use('/fr', appFr);
-  server.use('/pt', appPt);
-  server.use('/en', appEn);
-  server.use('', appEn);
+  const server = app();
   server.listen(port, () => {
     console.log(`Node Express server listening on http://localhost:${port}`);
   });
-}*/
+}
 
 // Webpack will replace 'require' with '__webpack_require__'
 // '__non_webpack_require__' is a proxy to Node 'require'
 // The below code is to ensure that the server is run only when not requiring the bundle.
-/*declare const __non_webpack_require__: NodeRequire;
+declare const __non_webpack_require__: NodeRequire;
 const mainModule = __non_webpack_require__.main;
 const moduleFilename = mainModule && mainModule.filename || '';
 if (moduleFilename === __filename || moduleFilename.includes('iisnode')) {
   run();
-}*/
+}
 
-export * from './src/main.server';
+export default bootstrap;
