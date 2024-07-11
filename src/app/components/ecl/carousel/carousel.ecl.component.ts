8c92d981b32ac1e4f71d043b47e5f857d71f3eb2
nikolay torbev <ntorbev@gmail.com>
2024-07-11 12:57:42 +0300

    ecl adjustment

--- src/app/components/ecl/carousel/carousel.ecl.component.ts
+++ src/app/components/ecl/carousel/carousel.ecl.component.ts
@@ -1,35 +1,45 @@
-import {AfterViewInit, Component, Inject, PLATFORM_ID} from '@angular/core';
-import {TranslateService} from "../../../services/translate.service";
-import {DOCUMENT, isPlatformBrowser} from "@angular/common";
+import { DOCUMENT, isPlatformBrowser } from '@angular/common';
+import { AfterViewInit, Component, Inject, PLATFORM_ID } from '@angular/core';
+import { TranslateService } from '../../../services/translate.service';
+
 declare let ECL: any;
 
 @Component({
-    selector: 'app-ecl-carousel',
-    templateUrl: './carousel.ecl.component.html',
-    styleUrls: ['./carousel.ecl.component.scss']
+  selector: 'app-ecl-carousel',
+  templateUrl: './carousel.ecl.component.html',
+  styleUrls: [ './carousel.ecl.component.scss' ]
 })
 
 export class CarouselEclComponent implements AfterViewInit {
 
-    public carousel: any;
+  public carousel: any;
 
-    constructor(public translateService: TranslateService,
-                @Inject(PLATFORM_ID) private platformId: Object,
-                @Inject(DOCUMENT) private _doc: Document) { }
+  constructor(public translateService: TranslateService,
+              @Inject(PLATFORM_ID) private platformId: Object,
+              @Inject(DOCUMENT) private _doc: Document) {
+  }
 
-    ngOnInit() {
+  ngOnInit() {
+  }
+
+  ngAfterViewInit(): void {
+    if (this.isPlatformBrowser()) {
+      let elt = this._doc.querySelector('[data-ecl-carousel]');
+      this.carousel = new ECL.Carousel(elt);
+
+      this.carousel.init();
     }
+  }
 
-    ngAfterViewInit(): void {
-        if (this.isPlatformBrowser()) {
-          let elt = this._doc.querySelector('[data-ecl-carousel]');
-          this.carousel = new ECL.Carousel(elt);
-          this.carousel.init();
-        }
-    }
+  isPlatformBrowser() {
+    return isPlatformBrowser(this.platformId);
+  }
 
-    isPlatformBrowser(){
-      return isPlatformBrowser(this.platformId);
-    }
+  next() {
+    this.carousel.shiftSlide('next', true);
+  }
 
+  previous() {
+    this.carousel.shiftSlide('prev', true);
+  }
 }
