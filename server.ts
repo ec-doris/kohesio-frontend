e2e46caf62372ea15758813ea3cc2e8f439acb0f
nikolay torbev <ntorbev@gmail.com>
2024-07-18 00:23:38 +0300

    distfolder correct

--- server.ts
+++ server.ts
@@ -13,12 +13,13 @@ import { LOCALE_ID } from '@angular/core';
 // The Express app is exported so that it can be used by serverless Functions.
 export function app(): express.Express {
   const server = express();
-  const lang = 'en';
+  // const lang = 'en';
 
   // @ts-ignore
-  const dir = process.env.DIST_DIR || join(process.cwd(), 'dist/kohesio-frontend/browser');
+  // const dir = process.env.DIST_DIR || join(process.cwd(), 'dist/kohesio-frontend/browser');
   // const distFolder = join(process.cwd(), 'dist/kohesio-frontend/browser');
-  const distFolder = join(dir ? dir : process.cwd(), `${lang}`);
+  // const distFolder = join(dir ? dir : process.cwd(), `${lang}`);
+  const distFolder = join(process.cwd(), 'dist/kohesio-frontend/browser');
 
   // const indexHtml = existsSync(join(distFolder, 'index.original.html'))
   //   ? join(distFolder, 'index.original.html')
